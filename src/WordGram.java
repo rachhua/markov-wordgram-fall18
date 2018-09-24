f2ce5055917e896416b7110561a43bda0ac06a87
Rachel Hua <rachel.hua@duke.edu>
2018-09-24 10:09:10 -0400

    done finally

--- src/WordGram.java
+++ src/WordGram.java
@@ -1,12 +1,15 @@
+import java.util.Arrays;
+
+
 /**
  * WordGram objects represent a k-gram of strings/words.
  * 
- * @author YOUR-NAME
+ * @author Rachel Hua
  *
  */
 
 public class WordGram {
-	
+
 	private String[] myWords;   
 	private String myToString;  // cached string
 	private int myHash;         // cached hash value
@@ -19,7 +22,12 @@ public class WordGram {
 	 */
 	public WordGram(String[] source, int start, int size) {
 		myWords = new String[size];
+		for (int i = 0; i < size; i++) {
+			myWords[i] = source[start + i];
+		}	
 		// TODO: initialize myWords and ...
+		myToString = null;
+		myHash = 0;
 	}
 
 	/**
@@ -39,27 +47,45 @@ public class WordGram {
 	 * @return
 	 */
 	public int length(){
-		// TODO: change this
-		return 0;
+		//TODO: change this
+		return myWords.length;
+
 	}
 
 
 	@Override
+	//first check if obj is WordGram (cast it or not)
+	//then check if they're the same length
+	//then make a forloop and check it all inside
+	//then return false at the end if it doesn't work
+
 	public boolean equals(Object o) {
-		if (! (o instanceof WordGram) || o == null){
+		if (!(o instanceof WordGram || o == null)) {
 			return false;
 		}
-
-	    // TODO: complete this method
+		WordGram wg = (WordGram) o;
+		if(wg.length() != this.length()) {
+			return false;
+		}		
+		for(int i = 0; i < myWords.length; i++) {
+			if (!(myWords[i].equals(wg.wordAt(i)))) {
+				return false;
+			}
+		}
 		return true;
 	}
 
+	//CORRECT: checked by TA
 	@Override
-	public int hashCode(){
+	public int hashCode() {
 		// TODO: complete this method
-		return myHash;
+		if (myHash == 0) {
+			myHash = toString().hashCode();
+		}
+		return myHash; 
+
 	}
-	
+
 
 	/**
 	 * Create and complete this comment
@@ -69,12 +95,24 @@ public class WordGram {
 	public WordGram shiftAdd(String last) {
 		WordGram wg = new WordGram(myWords,0,myWords.length);
 		// TODO: Complete this method
+		for(int i = 0; i< myWords.length-1; i++) {
+			wg.myWords[i] = wg.myWords[i+1];
+		}
+		wg.myWords[myWords.length-1]=last;
 		return wg;
 	}
 
 	@Override
+	//CORRECT: checked by TA
 	public String toString(){
-		// TODO: Complete this method	
+		// TODO: Complete this method
+		if (myToString == null) {
+			myToString = String.join(" ", myWords);
+		}
 		return myToString;
 	}
 }
+
+
+
+
